847aa38ccf149d7fd707ee525248f7bbb89ce3a6
Derk Braakman <dbraakman@bol.com>
2016-11-05 23:37:02 +0100

    chore(releasing): Add travis config, add test, conventional commit and sematic release
    closes #1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import uniqueRandomArray from 'unique-random-array';
-import starWarsNames from './starwars-names.json';
-
-const getRandomItem = uniqueRandomArray(starWarsNames);
-
-module.exports = {
-  all: starWarsNames,
-  random: random
-};
-
-function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    const randomItems = [];
-    for (let i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
-    }
-    return randomItems;
-  }
-}
-=======
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
 
@@ -28,4 +5,3 @@ module.exports = {
   all: starWarsNames,
   random: uniqueRandomArray(starWarsNames)
 };
->>>>>>> lesson/02-setting-up-github
